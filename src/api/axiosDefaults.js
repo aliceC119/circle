690934f1fb94da8ca3f6fe261c2cccf529171b9e
Alice Chan <alicebay10@yahoo.co.uk>
2025-01-18 19:39:46 +0000

    created a SignUpForm.js and applied styles to the form.

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://drf-api-circle-320cd30d1665.herokuapp.com/';
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
